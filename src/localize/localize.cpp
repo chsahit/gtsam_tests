a189641242df2d4ea4bb5c72e1a97444f400efef
Sahit Chintalapudi <chsahit@gmail.com>
2017-07-22 20:01:45 -0400

    added all the logic, message types and covariances are still todo

--- src/localize/localize.cpp
+++ src/localize/localize.cpp
@@ -1,13 +1,33 @@
 #include <ros/ros.h>
+#include <math.h>
+#include <gtsam/geometry/Pose2.h>
+#include <gtsam/inference/Key.h>
+#include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam/nonlinear/Values.h>
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
 
 ros::Publisher posePub;
 
-void encoderCB() {
+gtsam::NonlinearFactorGraph graph;
+int nextPose = 2;
 
+float lastAngle = 0;
+float lastEncoder = 0;
+
+void angleCB() {
+    lastAngle = 2/*angle*/;
+}
+
+void encoderCB() {
+    lastEncoder = 2/*enc*/;
 }
 
 void predictPose() {
-
+    gtsam::Pose2 odometry(lastEncoder * sin(lastAngle), lastEncoder * cos(lastAngle), lastAngle);
+    gtsam::noiseModel::Diagonal::shared_ptr odometryNoise = noiseModel::Diagonal::Sigmas(Vector_(3, 0.2, 0.2, 0.1));
+    graph.add(gtsam::BetweenFactor<gtsam::Pose2>(nextPose - 1, nextPose, odometry, odometryNoise));
+    nextPose++;
 }
 
 int main(int argc, char **argv) {
@@ -15,7 +35,12 @@ int main(int argc, char **argv) {
     ros::NodeHandle nh;
 
     auto encoderSub = nh.subscribe("/encoder", 1, encoderCB);
+    auto angleSub = nh.subscribe("/angle", 1, angleCB);
     posePub = nh.advertise<<Pose2>>("/pose", 1);
+
+    gtsam::Pose2 priorMean(0.0, 0.0, 0.0);
+    gtsam::noiseModel::Diagonal::shared_ptr priorNoise = gtsam::noiseModel::Diagonal::Sigmas(Vector_(3, 0.2, 0.2, 0.1));
+    graph.add(gtsam::PriorFactor<gtsam::Pose2>(1, priorMean, priorNoise));
 
     ros::Rate rate(30.0);
     while (ros::ok()) {
